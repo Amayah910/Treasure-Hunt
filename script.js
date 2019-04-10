813502c677dce079786b68d171c093fd7a6314ee
Amayah910 <43344691+Amayah910@users.noreply.github.com>
2019-04-10 09:43:34 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+$("button").click(function() {
+    $(".image2").show();
+    $(".button_div").show();
+    $("button").text(" double Click the bird");
+});
+
+$(".image1").click(function(){
+    $(".image1").show();
+});
+
+$(".image2").dblclick(function() {
+    $("body").css("background-color", "lightpink");
+});
+
+    $(".image1").click(function() {
+    $("body").text("You've made it to the end!");
+});
+
+
+    
+
+    
+
